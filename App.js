ba28ddd60d99bbc0f3092fdf3c367b71cf809466
Aliaksandr Puhachou <a.puhachou@21vek.by>
2021-12-28 23:54:49 +0300

    Added react navigate with mainscreens

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
-import { Text, View } from 'react-native';
 import {AppLoading} from 'expo';
 import {bootstrap} from './src/bootstrap';
+import { AppNavigation } from './src/navigation/AppNavigation';
 
 export default function App() {
   const [isReady, setIsReady] = useState(false);
@@ -17,8 +17,6 @@ export default function App() {
   }
 
   return (
-    <View>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <AppNavigation />
   );
 }
---
+++ src/screens/BookedScreen.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import {View, Text, StyleSheet} from 'react-native'
+
+export const BookedScreen = ({}) => {
+
+    return (
+        <View style={styles.center}>
+            <Text>
+                BookedScreen
+            </Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    center: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+})
