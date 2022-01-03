de5f4209c6d41c0af8e76de7db872f3c1f628ae7
Aliaksandr Puhachou <a.puhachou@21vek.by>
2022-01-04 00:04:38 +0300

    Added style for Post

--- src/screens/PostScreen.js
+++ src/screens/PostScreen.js
@@ -1,15 +1,45 @@
 import React from 'react'
-import {View, Text, StyleSheet} from 'react-native'
+import {View, StyleSheet, Text, Image, Button, ScrollView, Alert} from 'react-native'
+import { DATA } from '../data'
+import { THEME } from '../theme';
 
 export const PostScreen = ({ navigation }) => {
     const postId = navigation.getParam('postId');
 
+    const post = DATA.find(p => p.id === postId)
+
+    const removeHandler = () => {
+        Alert.alert(
+            "Удаление поста",
+            "Вы уверены?",
+            [
+                {
+                    text: "Отменить",
+                    onPress: () => console.log("Cancel Pressed"),
+                    style: "cancel"
+                },
+                { 
+                    text: "Да", 
+                    style: "destructive",
+                    onPress: () => console.log("OK Pressed") 
+                }
+            ]
+        );
+    }
+
     return (
-        <View style={styles.center}>
-            <Text>
-                {postId}
-            </Text>
-        </View>
+        <ScrollView>
+            <Image 
+                source={{ uri: post.img }}
+                style={styles.image}
+            />
+            <View style={styles.textWrap}>
+                <Text style={styles.title}>
+                    {post.text}
+                </Text>
+            </View>
+            <Button title='Удалить' onPress={removeHandler} color={THEME.DANGER_COLOR} />
+        </ScrollView>
     )
 }
 
@@ -22,9 +52,14 @@ PostScreen.navigationOptions = ({ navigation }) => {
 }
 
 const styles = StyleSheet.create({
-    center: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
+    image: {
+        width: '100%',
+        height: 200
+    },
+    textWrap: {
+        padding: 10
+    },
+    title: {
+        fontFamily: 'open-regular'
     }
 })
