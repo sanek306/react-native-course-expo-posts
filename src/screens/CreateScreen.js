d61d19a10783ac2a6f9e496a47df5056c1b84314
Aliaksandr Puhachou <a.puhachou@21vek.by>
2022-02-03 00:09:24 +0300

    added function Add Post

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -1,8 +1,14 @@
-import React from 'react'
-import {View, Text, StyleSheet} from 'react-native'
+import React, { useState } from 'react'
+import {View, Text, StyleSheet, TextInput, Image, Button, ScrollView, TouchableWithoutFeedback, Keyboard } from 'react-native'
+import { useDispatch } from 'react-redux';
 import { navigationOptions } from '../navigationOptions';
+import { addPost } from '../store/actions/post';
+import { THEME } from '../theme';
 
 export const CreateScreen = ({ navigation }) => {
+    const [text, setText] = useState('')
+    const dispatch = useDispatch();
+
     React.useLayoutEffect(() => {
         navigation.setOptions({
             title: 'Создать пост',
@@ -10,20 +16,62 @@ export const CreateScreen = ({ navigation }) => {
         });
     }, [navigation]);
 
+    const saveHandler = () => {  
+        dispatch(addPost({ 
+            img: 'https://static.coindesk.com/wp-content/uploads/2019/01/shutterstock_1012724596-860x430.jpg',
+            date: new Date().toJSON(),
+            text,
+            booked: false
+        }))
+
+        navigation.navigate('Main');
+    }
 
     return (
-        <View style={styles.center}>
-            <Text>
-                CreateScreen
-            </Text>
-        </View>
+        <ScrollView >
+            <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+                <View style={styles.wrapper}>
+                    <Text style={styles.title}>
+                        Создай новый пост
+                    </Text>
+                    <TextInput 
+                        style={styles.textArea}
+                        placeholder='Введите текст поста'
+                        value={text}
+                        onChangeText={setText}
+                        multiline
+                    />
+                    <Image 
+                        style={styles.img}
+                        source={{ 
+                            uri: 'https://static.coindesk.com/wp-content/uploads/2019/01/shutterstock_1012724596-860x430.jpg'
+                        }}
+                    />
+                    <Button title='Создать пост' color={THEME.MAIN_COLOR} onPress={saveHandler} />
+                </View>
+            </TouchableWithoutFeedback>
+        </ScrollView>
     )
 }
 
 const styles = StyleSheet.create({
-    center: {
+    wrapper: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
+        alignItems: 'center',
+        padding: 10,
+    },
+    title: {
+        fontSize: 20,
+        fontFamily: 'open-regular',
+        marginVertical: 10
+    },
+    textArea: {
+        width: '100%',
+        paddingVertical: 10
+    },
+    img: { 
+        width: '100%',
+        height: 200,
+        marginBottom: 10
     }
 })
