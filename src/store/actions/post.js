2545ba9080ac710b7b3042feb372eb08c65236bc
Aliaksandr Puhachou <a.puhachou@21vek.by>
2022-02-06 19:04:19 +0300

    added createPost in db

--- src/store/actions/post.js
+++ src/store/actions/post.js
@@ -1,9 +1,15 @@
+import * as FileSystem from 'expo-file-system';
+import { DB } from "../../db"
 import { ADD_POST, LOAD_POSTS, REMOVE_POST, TOGGLE_BOOKED } from "../types"
 
 export const loadPosts = () => {
-    return {
-        type: LOAD_POSTS,
-        payload: []
+    return async dispatch => {
+        const posts = await DB.getPosts()
+
+        dispatch({
+            type: LOAD_POSTS,
+            payload: posts
+        })
     }
 }
 
@@ -21,11 +27,28 @@ export const removePost = (id) => {
     }
 }
 
-export const addPost = (post) => {
-    post.id = Date.now().toString();
+export const addPost = post => async dispatch => {
+    const fileName = post.img.split('/').pop()
+    const newPath = FileSystem.documentDirectory + fileName
 
-    return {
-        type: ADD_POST,
-        payload: post
+    try {
+        await FileSystem.moveAsync({
+            to: newPath,
+            from: post.img
+        })
     }
+    catch (e) {
+        console.log(e)
+    }
+
+    const payload = {...post, img: newPath}
+
+    const id = await DB.createPost(payload)
+
+    payload.id = id
+
+    dispatch({
+        type: ADD_POST,
+        payload
+    })
 }
