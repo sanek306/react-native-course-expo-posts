f670cc8651e633b6d47cc7b73cf9f8c74a6e702f
Aliaksandr Puhachou <a.puhachou@21vek.by>
2022-02-06 19:16:37 +0300

    added loading indicator

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,14 +1,18 @@
 import React, { useEffect } from 'react'
+import { View, StyleSheet } from 'react-native';
+import { ActivityIndicator } from 'react-native-paper';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import { useDispatch, useSelector } from 'react-redux';
 import { AppHeaderIcon } from '../components/AppHeaderIcon';
 import { PostList } from '../components/PostList';
 import { navigationOptions } from '../navigationOptions';
 import { loadPosts } from '../store/actions/post';
+import { THEME } from '../theme';
 
 export const MainScreen = ({ navigation }) => {
     const dispatch = useDispatch();
     const allPosts = useSelector(state => state.posts.allPosts);
+    const loading = useSelector(state => state.posts.loading);
     
     useEffect(() => {
         dispatch(loadPosts());
@@ -31,7 +35,23 @@ export const MainScreen = ({ navigation }) => {
         });
     }, [navigation]);
 
+    if (loading) {
+        return (
+            <View style={styles.center}>
+                <ActivityIndicator color={THEME.MAIN_COLOR}/>
+            </View>
+        )
+    }
+
     return (
         <PostList data={allPosts} onClick={openPostHandler} />
     ) 
 }
+
+const styles = StyleSheet.create({
+    center: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+})
